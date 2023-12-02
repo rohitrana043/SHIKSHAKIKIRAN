f478bce9912c6133f836380c27998f1e344283e1
ROHIT RANA <rohit.rana043@gmail.com>
2023-12-02 10:06:36 -0500

    adding postgres as database

--- shikshakikiran/settings.py
+++ shikshakikiran/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 import os
 import json
 # import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -88,11 +89,15 @@ WSGI_APPLICATION = 'shikshakikiran.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+DATABASE_URL='postgresql://postgres:DDF5G514B54dD241c-efa*AEA2d3-cF5@viaduct.proxy.rlwy.net:17786/railway'
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
+    "default": dj_database_url.config(default=DATABASE_URL, conn_max_age=1800),
 }
 
 
